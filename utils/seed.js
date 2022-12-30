1cead1169c8602b709b26eba2f063c4cec5e3fcb
Marcus mbp <maherreraiv1987@gmail.com>
2022-12-29 18:34:22 -0700

    removed commented code from seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -9,45 +9,6 @@ connection.once('open', async () => {
 
 	await Trail.deleteMany({});
 
-	// const trails = [
-	// 	{
-	// 		trailName: 'Emerald Lake Trail',
-	// 		description:
-	// 			"Enjoy this 3.2-mile out-and-back trail near Estes Park, Colorado. Generally considered an easy route, it takes an average of 1 h 39 min to complete. This is a very popular area for hiking and snowshoeing, so you'll likely encounter other people while exploring. The best times to visit this trail are June through October. You'll need to leave pups at home — dogs aren't allowed on this trail.",
-	// 		trailheadCoordinates: '40.31195,-105.64567',
-	// 		length: 3.2,
-	// 		elevationGain: 698,
-	// 		routeType: 'Out & Back',
-	// 		difficulty: 'Easy',
-	// 		image:
-	// 			'https://s27363.pcdn.co/wp-content/uploads/2020/09/Nymph-Dream-and-Emerald-Lake-Hike.jpg.optimal.jpg'
-	// 	},
-	// 	{
-	// 		trailName: 'Sky Pond via Glacier Gorge Trail',
-	// 		description:
-	// 			"Experience this 9.4-mile out-and-back trail near Estes Park, Colorado. Generally considered a challenging route, it takes an average of 4 h 32 min to complete. This is a very popular area for hiking and snowshoeing, so you'll likely encounter other people while exploring. The best times to visit this trail are June through October. You'll need to leave pups at home — dogs aren't allowed on this trail.",
-	// 		trailheadCoordinates: '40.31052,-105.6403',
-	// 		length: 9.4,
-	// 		elevationGain: 1758,
-	// 		routeType: 'Out & Back',
-	// 		difficulty: 'Hard',
-	// 		image:
-	// 			'https://dayhikesneardenver.com/wp-content/uploads/2016/03/00-Sky-Pond-in-Rocky-Mountain-National-Park-bratman-creative-commons.jpg'
-	// 	},
-	// 	{
-	// 		trailName: 'The Loch via Glacier Gorge Trail',
-	// 		description:
-	// 			"Check out this 5.4-mile out-and-back trail near Estes Park, Colorado. Generally considered a moderately challenging route, it takes an average of 2 h 39 min to complete. This is a very popular area for birding, hiking, and snowshoeing, so you'll likely encounter other people while exploring. The best times to visit this trail are March through October. You'll need to leave pups at home — dogs aren't allowed on this trail.",
-	// 		trailheadCoordinates: '40.31052,-105.6403',
-	// 		length: 5.4,
-	// 		elevationGain: 1056,
-	// 		routeType: 'Out & Back',
-	// 		difficulty: 'Hard',
-	// 		image:
-	// 			'https://www.nps.gov/common/uploads/cropped_image/primary/CEB6EA70-0883-683E-21A9C3F5034B93D9.jpg?width=1200&quality=90&mode=crop'
-	// 	}
-	// ];
-
 	await Trail.collection.insertMany(trails);
 
 	console.table(trails);
