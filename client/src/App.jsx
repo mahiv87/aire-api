d0325a0057af04def958801240e90d12c4bc9fbe
Marcus Herrera <maherreraiv1987@gmail.com>
2022-12-31 09:30:25 -0700

    added chakra ui loading spinner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Spinner } from '@chakra-ui/react';
 import aireLogo from './assets/nature_icon.svg';
 import './App.css';
 
@@ -20,44 +22,50 @@ function App() {
 	}, [getTrails]);
 
 	return (
-		<div className="App">
-			<img className="logo" src={aireLogo} alt="" />
-			<h1 className="title">aire</h1>
-			{response.length > 1 ? (
-				<div className="trailCard">
-					<img className="trailImg" src={response[0].image} alt="" />
-					<h2 className="trailTitle">{response[0].trailName}</h2>
-					<div className="trailInfo">
-						<p className="trailDifficulty">
-							Difficulty: <span>{response[0].difficulty}</span>
-						</p>
-						<p className="trailLength">
-							Length: <span>{response[0].length}mi</span>
-						</p>
-						<p className="trailType">
-							Route: <span>{response[0].routeType}</span>
-						</p>
-						<p className="trailElevation">
-							Elevation Gain: <span>{response[0].elevationGain}ft</span>
-						</p>
+		<ChakraProvider>
+			<div className="App">
+				<header>
+					<img className="logo" src={aireLogo} alt="" />
+					<h1 className="title">aire</h1>
+				</header>
+				{response.length > 1 ? (
+					<div className="trailCard">
+						<img className="trailImg" src={response[0].image} alt="" />
+						<h2 className="trailTitle">{response[0].trailName}</h2>
+						<div className="trailInfo">
+							<p className="trailDifficulty">
+								Difficulty: <span>{response[0].difficulty}</span>
+							</p>
+							<p className="trailLength">
+								Length: <span>{response[0].length}mi</span>
+							</p>
+							<p className="trailType">
+								Route: <span>{response[0].routeType}</span>
+							</p>
+							<p className="trailElevation">
+								Elevation Gain: <span>{response[0].elevationGain}ft</span>
+							</p>
+						</div>
+						<div></div>
+						<article className="trailDescription">
+							{response[0].description}
+						</article>
+						<div className="trailhead">
+							<a
+								href={`https://www.google.com/maps/place/${response[0].trailheadCoordinates}`}
+								target="_blank"
+							>
+								Trailhead
+							</a>
+						</div>
 					</div>
-					<div></div>
-					<article className="trailDescription">
-						{response[0].description}
-					</article>
-					<div className="trailhead">
-						<a
-							href={`https://www.google.com/maps/place/${response[0].trailheadCoordinates}`}
-							target="_blank"
-						>
-							Trailhead
-						</a>
+				) : (
+					<div className="spinner">
+						<Spinner size="xl" />
 					</div>
-				</div>
-			) : (
-				<h2>Loading...</h2>
-			)}
-		</div>
+				)}
+			</div>
+		</ChakraProvider>
 	);
 }
 
