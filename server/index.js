73b5e4caf4b6770fa4e0d0aac45e5a80a512665f
Marcus mbp <maherreraiv1987@gmail.com>
2023-01-03 17:57:42 -0700

    debugging routes

--- server/index.js
+++ server/index.js
@@ -12,6 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/public')));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
 db.once('open', () => {
 	app.listen(PORT, () => {
 		console.log(`API server running on port ${PORT}`);
